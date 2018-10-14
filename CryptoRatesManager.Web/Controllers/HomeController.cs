2fc5ba7654dca6070b9c07e815266c1fb8b4cc97
Sergey Korotunov <korotunovsergey@gmail.com>
2018-10-14 13:24:59 +0300

    Clean-up unused files and add required references

--- CryptoRatesManager.Web/Controllers/HomeController.cs
+++ CryptoRatesManager.Web/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using CryptoRatesManager.Core.Providers;
 using CryptoRatesManager.Core.Services.Abstract;
 using CryptoRatesManager.Core.Services.Concrete;
-using CryptoRatesManager.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace CryptoRatesManager.Web.Controllers
 {
@@ -22,32 +20,7 @@ namespace CryptoRatesManager.Web.Controllers
                 },
                 1);
             service.StartExecution();
-            return this.View();
-        }
-
-        public IActionResult About()
-        {
-            this.ViewData["Message"] = "Your application description page.";
-
-            return this.View();
-        }
-
-        public IActionResult Contact()
-        {
-            this.ViewData["Message"] = "Your contact page.";
-
-            return this.View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
